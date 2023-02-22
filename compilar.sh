b5acd70c7e8990f7c13b947c633d40edb7bd580d
Pablo Caiza <pablocuri2015@hotmail.com>
2023-02-21 20:31:04 -0500

    dockerhub

--- compilar.sh
+++ compilar.sh
@@ -4,15 +4,15 @@ cd library_microservices_v3
 #book microservice
 cd app-books
 ./gradlew libertyPackage
-docker build -t pjcaiza/app-books:1.0.0
+docker build -t yiecheverria/app-books:1.0.0
 cd ..
 #author microservice 
 cd app-authors
-docker build -t pjcaiza/app-authors:1.0.0
+docker build -t yiecheverria/app-authors:1.0.0
 cd ..
 #push docker hub
-docker push pjcaiza/app-books:1.0.0
-docker push pjcaiza/app-authors:1.0.0
+docker push yiecheverria/app-books:1.0.0
+docker push yiecheverria/app-authors:1.0.0
 #run docker compose 
 docker compose up
 
